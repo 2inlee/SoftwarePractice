4e2f9dd4925f922f41c6163a8c71cd03f37a1a6f
Nam Jung Hyun <nam9490@gmail.com>
2023-11-23 15:59:48 +0900

    add plt code

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data from CSV file
 file_path = "cox-violent-parsed_filt_usable.csv"
@@ -18,3 +19,27 @@ print(df['age_cat'].value_counts())
 
 # Display the distribution of the 'sex' column
 print(df['sex'].value_counts())
+
+# Plotting the distribution of the 'race' column
+plt.figure(figsize=(10, 5))
+df['race'].value_counts().plot(kind='bar', color='skyblue')
+plt.title('Distribution of Race')
+plt.xlabel('Race')
+plt.ylabel('Count')
+plt.show()
+
+# Plotting the distribution of the 'age_cat' column
+plt.figure(figsize=(10, 5))
+df['age_cat'].value_counts().plot(kind='bar', color='lightcoral')
+plt.title('Distribution of Age Categories')
+plt.xlabel('Age Category')
+plt.ylabel('Count')
+plt.show()
+
+# Plotting the distribution of the 'sex' column
+plt.figure(figsize=(8, 5))
+df['sex'].value_counts().plot(kind='bar', color='lightgreen')
+plt.title('Distribution of Sex')
+plt.xlabel('Sex')
+plt.ylabel('Count')
+plt.show()
